02a01a507ab8769f875052eba95ff2b955408408
Lucas Marcondes <lucas.marcondes36@gmail.com>
2022-06-21 18:56:10 -0300

    Upload dadosFormacaoAcademica.js

---
+++ script/dadosFormacaoAcademica.js
@@ -0,0 +1,83 @@
+const formacaoAcademicaDados = [
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4E0BAQEz5mZ_QcLylw/company-logo_100_100/0/1644871045148?e=1663804800&v=beta&t=U9zeoiEapY-7kGvNrRYn7VkRELyoFHDwarkHVKZSLWY',
+        titulo: 'universidade paulista',
+        paragrafo: [
+            'Bacharelado, Ciência da Computação',
+            'mar de 2022 - 2025'
+        ],
+        descricao: []
+    },
+    {
+        img: 'https://static-exp2.licdn.com/sc/h/8zzzkhxduv0r11cuxbs48pg03',
+        titulo: 'escola estadual professor eusébio de paula marcondes',
+        paragrafo: [
+            'Ensino Médio',
+            '2019 - 2021'
+        ],
+        descricao: [
+            'Nota: Concluído'
+        ]
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4E0BAQFB2gc7ymRxYQ/company-logo_100_100/0/1637066343095?e=1663804800&v=beta&t=Eiy2eNFya3j2SUnmlppttnKZBMKf4Un3H5r94GIe4IY',
+        titulo: 'CEAP - centro educacional assistencial profissionalizante',
+        paragrafo: [
+            'Samsung Innovation Campus • 1 ano, Programação',
+            '2020 - 2020'
+        ],
+        descricao: [
+            'Nota: Concluído',
+            '- Curso disponibilizado pelo o CEAP em parceria com a Samsung.',
+            '- Conteúdo estudado: Python, C#, Arduino.'
+        ]
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4E0BAQFB2gc7ymRxYQ/company-logo_100_100/0/1637066343095?e=1663804800&v=beta&t=Eiy2eNFya3j2SUnmlppttnKZBMKf4Un3H5r94GIe4IY',
+        titulo: 'CEAP - centro educacional assistencial profissionalizante',
+        paragrafo: [
+            'Técnico em Informática • 2 anos',
+            '2019 - 2020'
+        ],
+        descricao: [
+            'Nota: Concluído',
+            '- Conteúdo estudado: Java, Python, Banco de Dados (mysql), Arduino, JavaScript (Vanilla), HTML e CSS.'
+        ]
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4E0BAQFB2gc7ymRxYQ/company-logo_100_100/0/1637066343095?e=1663804800&v=beta&t=Eiy2eNFya3j2SUnmlppttnKZBMKf4Un3H5r94GIe4IY',
+        titulo: 'CEAP - centro educacional assistencial profissionalizante',
+        paragrafo: [
+            'Informática Aplicada • 1 ano',
+            '2018 - 2018'
+        ],
+        descricao: [
+            'Nota: Concluído',
+            'Conteúdo estudado: Java, Python, HTML e CSS.'
+        ]
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4E0BAQFB2gc7ymRxYQ/company-logo_100_100/0/1637066343095?e=1663804800&v=beta&t=Eiy2eNFya3j2SUnmlppttnKZBMKf4Un3H5r94GIe4IY',
+        titulo: 'CEAP - centro educacional assistencial profissionalizante',
+        paragrafo: [
+            'Informática Básica • 1 ano',
+            '2017 - 2017'
+        ],
+        descricao: [
+            'Nota: Concluído',
+            'Conteúdo estudado: Arduino, HTML e CSS.'
+        ]
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4E0BAQFB2gc7ymRxYQ/company-logo_100_100/0/1637066343095?e=1663804800&v=beta&t=Eiy2eNFya3j2SUnmlppttnKZBMKf4Un3H5r94GIe4IY',
+        titulo: 'CEAP - centro educacional assistencial profissionalizante',
+        paragrafo: [
+            'Eletricidade Industrial • 1 ano',
+            '2016 - 2016'
+        ],
+        descricao: [
+            'Nota: Concluído',
+            'Conteúdo estudado: Arduino e Pacote Office (Word, Excel e PowerPoint).'
+        ]
+    }
+]
