1aeb8da796b8c10c0f2b2bb59a16a6df98d07e42
Lucas Marcondes <lucas.marcondes36@gmail.com>
2022-06-21 18:57:43 -0300

    Upload dadosLicencasECertificados.js

---
+++ script/dadosLicencasECertificados.js
@@ -0,0 +1,317 @@
+const licencasECertificadosDados = [
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Comunicação não violenta: consciência para agir',
+        paragrafo: [
+            'Alura',
+            'Emitido em jun de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/d21a55e0-9c94-4b89-ab76-8c387c55a12a'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'JS na web: CRUD com JavaScript assíncrono',
+        paragrafo: [
+            'Alura',
+            'Emitido em jun de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/041fa1eb-e7fa-4aa3-be79-f6e761475664'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'JavaScript na Web: validação de Formulários e HTML5',
+        paragrafo: [
+            'Alura',
+            'Emitido em jun de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/c8bc5c11-f57e-418a-a61e-6debf73897c5'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'JavaScript: explorando a linguagem',
+        paragrafo: [
+            'Alura',
+            'Emitido em jun de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/a743d690-97b4-412e-a160-12bdc8f09e3b'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'JavaScript: programando a Orientação a Objetos',
+        paragrafo: [
+            'Alura',
+            'Emitido em jun de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/3900be27-52f6-435a-8302-2a96b18ee080'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Comunicação: como se expressar bem e ser compreendido',
+        paragrafo: [
+            'Alura',
+            'Emitido em abr de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/e224ec88-8472-4b50-9905-493f87b1a10a'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Foco: trazendo mais resultados para o dia a dia',
+        paragrafo: [
+            'Alura',
+            'Emitido em abr de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/790e40a8-a8f6-4b00-bd5b-1d07f0fe11b0'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Hábitos: da produtividade às metas pessoais',
+        paragrafo: [
+            'Alura',
+            'Emitido em abr de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/03f3012f-0a36-4f81-8cc1-25868909efa7'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'JS na Web: Armazenando dados no navegador',
+        paragrafo: [
+            'Alura',
+            'Emitido em abr de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/96e29788-2a7f-4aa6-bf94-3f185391a60a'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'NodeJS: avançando em APIs REST com controle de versões',
+        paragrafo: [
+            'Alura',
+            'Emitido em abr de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/2a221d32-55ff-4b49-bce2-22bf8cc313d3'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'ORM com NodeJS: API com Sequelize e MySQL',
+        paragrafo: [
+            'Alura',
+            'Emitido em abr de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/c143c0f8-87e7-4d05-85bd-35f5439c055b'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'ORM com NodeJS: avançando nas funcionalidades do Sequelize',
+        paragrafo: [
+            'Alura',
+            'Emitido em abr de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/3e7a32ad-ac13-4a14-a8f0-d6ed922aa357'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Fundamentos do JavaScript: Arrays',
+        paragrafo: [
+            'Alura',
+            'Emitido em mar de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/57638a65-ac45-49b1-a2d4-b39b68b5506a'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Fundamentos do JavaScript: Tipos, variáveis e funções',
+        paragrafo: [
+            'Alura',
+            'Emitido em mar de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/62f1fc7c-ba85-455a-a495-c2e0eda18bcb'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Fundamentos do JavaScript: objetos',
+        paragrafo: [
+            'Alura',
+            'Emitido em mar de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/dabbd13a-f03f-40fb-b6f7-142018edf7d4'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'HTTP: Entendendo a web por baixo dos panos',
+        paragrafo: [
+            'Alura',
+            'Emitido em mar de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/dd775d41-3126-46e6-b1b5-0887654bccf5'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Introdução ao SQL com MySQL: manipule e consulte dados',
+        paragrafo: [
+            'Alura',
+            'Emitido em mar de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/f063c578-f57a-4c4e-90ec-5f6eec10b04d'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'NodeJS: Streaming de dados e Repositório',
+        paragrafo: [
+            'Alura',
+            'Emitido em mar de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/19f56a1e-c2e4-43d8-aa6d-b393aed92431'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'NodeJS: crie uma API REST padronizada e escalável',
+        paragrafo: [
+            'Alura',
+            'Emitido em mar de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/4fc213d7-def1-421e-b621-f8d07e6953cf'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Rest com NodeJS: API com Express e MySQL',
+        paragrafo: [
+            'Alura',
+            'Emitido em mar de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/bb1e8ea9-629d-4a5b-b0ed-54aa08fce024'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Flexbox: Posicione elementos na tela',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/180da792-bb2d-40b7-af55-4e42930213d4'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Git e Github: Controle e compartilhe seu código',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/7d115220-50ad-4856-b739-66e9cbd496b4'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'HTML5 e CSS3 parte 1: A primeira página da Web',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/228f0f81-926c-4ace-95d1-46d922bfb928'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'HTML5 e CSS3 parte 2: Posicionamento, listas e navegação',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/101be1d7-9ee1-4279-be88-f03b0e384368'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'HTML5 e CSS3 parte 3: Trabalhando com formulários e tabelas',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/e9e44dca-7593-4564-949d-aa29e375cb7e'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'HTML5 e CSS3 parte 4: Avançando no CSS',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/b96a0f82-bd55-4126-ad87-445946530dca'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'JavaScript para Web: Crie páginas dinâmicas',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/e417f92a-d907-4561-b046-fb043b0b3d2e'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Jogos clássicos parte 1: Iniciando no Javascript com Pong',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/724312e6-1c9b-461c-bb7d-1cbf34a2b832'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Jogos clássicos parte 2: laços e listas com Javascript',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/b5cd63cf-2756-446b-bbf0-d2e42d4ba119'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'LGPD: Conhecendo e entendendo seus impactos',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/4220b509-eb38-4ced-adc7-0788af05c4cb'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Layouts Responsivos: Trabalhando com layouts mobile',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/ae110f8c-2cc2-4c54-8545-f36d90a56682'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Scratch: Programando seu jogo 2d Parte 1',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/587835fb-f5fc-4391-a951-8103d683a00f'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Arquitetura de computadores: Por trás de como seu programa funciona',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/8b9db18e-dcc0-48ec-aa8a-8c431f229c1c'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Lógica de programação I: Os primeiros programas com Javascript e HTML',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/507b2bb6-1a48-4a0e-92cd-9866a72ee1cd'
+    },
+    {
+        img: 'https://media-exp1.licdn.com/dms/image/C4D0BAQEkPb_YckN8NQ/company-logo_200_200/0/1655315590765?e=1663804800&v=beta&t=byqFDxU2X4jnUEIu6ogivVZ1Qi5Bu6HWJi84ko9aAWs',
+        titulo: 'Lógica de programação II: pratique com desenhos, animações e um jogo',
+        paragrafo: [
+            'Alura',
+            'Emitido em fev de 2022 · Sem data de expiração'
+        ],
+        link: 'https://cursos.alura.com.br/certificate/02d19f6e-3760-43ce-a937-f1ee25a9bb95'
+    }
+]
