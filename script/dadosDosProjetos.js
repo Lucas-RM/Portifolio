58acd5695db2b67ba2873cd5f43e0b2a57a89036
Lucas Marcondes <lucas.marcondes36@gmail.com>
2022-06-24 23:33:53 -0300

    Conversão das imagens dos projetos para o formato .jpg

--- script/dadosDosProjetos.js
+++ script/dadosDosProjetos.js
@@ -1,49 +1,49 @@
 const projetos = [
     {
         link: 'https://lucas-rm.github.io/alura-midi/',
-        img: './img/alura-midi.png',
+        img: './img/alura-midi.jpg',
         titulo: 'Alura MIDI',
         descricao: 'Site feito em curso da plataforma Alura com o objetivo de tocar sons de instrumentos quando clicar em um botão ou quando pressionar a tecla <b>SPACE</b>.'
     },
     {
         link: 'https://lucas-rm.github.io/projeto-apeperia/',
-        img: './img/projeto-apeperia.png',
+        img: './img/projeto-apeperia.jpg',
         titulo: 'projeto apeperia',
         descricao: 'Site feito em curso da plataforma Alura com intuito de aprender sobre <b>Layouts Responsivos (mobile)</b>.'
     },
     {
         link: 'https://lucas-rm.github.io/alura-flexbox/',
-        img: './img/alura-flexbox.png',
+        img: './img/alura-flexbox.jpg',
         titulo: 'alura flexbox',
         descricao: 'Site feito em curso da plataforma Alura com intuito de aprender sobre <b>Flexbox</b>.'
     },
     {
         link: 'https://lucas-rm.github.io/Projeto-Huddle/',
-        img: './img/Projeto-Huddle.png',
+        img: './img/Projeto-Huddle.jpg',
         titulo: 'projeto huddle',
         descricao: 'Projeto acadêmico feito em curso com objetivo de aplicar <b>posicionamento do conteúdo</b> do site e deixar o site <b>responsivo</b>, sendo compatível para celular, computador, ipads, entre outros dispositivos.'
     },
     {
         link: 'https://lucas-rm.github.io/relogio-online/',
-        img: './img/Projeto-Relogio.png',
+        img: './img/Projeto-Relogio.jpg',
         titulo: 'projeto relógio',
         descricao: 'Projeto com intuito de representar um relógio em um site.'
     },
     {
         link: 'https://lucas-rm.github.io/pesquisa_inclusao_digital/',
-        img: './img/Projeto-InclusaoDigital.png',
+        img: './img/Projeto-InclusaoDigital.jpg',
         titulo: 'projeto inclusão digital',
         descricao: 'Projeto acadêmico realizado em grupo na faculdade com a finalidade de compreender o processo de <b>Inclusão Digital</b> na sociedade.'
     },
     {
         link: 'https://lucas-rm.github.io/alura-store/',
-        img: './img/alura-store.png',
+        img: './img/alura-store.jpg',
         titulo: 'alura store',
         descricao: 'Site feito em curso da plataforma Alura com o objetivo de aprender sobre <b>CSS Grid</b> e a <b>simplicação de Layouts</b>.'
     },
     {
         link: 'https://lucas-rm.github.io/agenda/',
-        img: './img/projeto-agenda.png',
+        img: './img/projeto-agenda.jpg',
         titulo: 'projeto agenda',
         descricao: 'O projeto agenda é um site que as pessoas podem definir o ano, o mês, o dia e o horário de suas tarefas. Além disso, terá as funções de deletar e de marcar como concluído as tarefas finalizadas.'
     }
