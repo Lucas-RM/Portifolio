0fd5ffb31783d6d0c062954a9e3a2532d9f40b2e
Lucas Marcondes <lucas.marcondes36@gmail.com>
2024-04-25 14:41:26 -0300

    Modificações no arquivo main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 function mudaTitle(title) {document.title = title}
 
 function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault()
+    if (event != undefined && event.type === 'touchstart') event.preventDefault()
     const $menu = document.querySelector('.menu')
     const $icone = document.querySelector('.fas')
 
@@ -10,7 +10,7 @@ function toggleMenu(event) {
     $icone.classList.toggle('fa-xmark')
 
     const $active = $menu.classList.contains('active')
-    event.currentTarget.setAttribute('aria-expanded', $active)
+    if (event != undefined) event.currentTarget.setAttribute('aria-expanded', $active)
 
     if ($active) {
         document.documentElement.style.overflow = 'hidden'
@@ -39,6 +39,11 @@ function botaoFechar() {
     $sobreMim_cabecalho.style.animation = 'fadeOut 300ms linear'
 }
 
+function atualizarAnoRodape() {
+    const $anoRodape = document.querySelector('#anoAtual')
+    $anoRodape.innerHTML = new Date().getFullYear()
+}
+
 const $pageScroll = document.documentElement
 const $visible = document.querySelector('.visible')
 const $fundoDesfocado = document.querySelector('.fundoDesfocado')
@@ -46,8 +51,20 @@ const $linkSobreMim = document.querySelector('#linkSobreMim')
 const $sobreMim_cabecalho = document.querySelector('.sobreMim__cabecalho')
 const $botaoFechar = document.querySelector('#sobreMim__btnFechar')
 const $mobileMenu = document.querySelector('#btnMobileMenu')
+const $contatos = document.querySelector('#contatos')
 let pageTitle = document.title
 
 $mobileMenu.addEventListener('click', toggleMenu)
+$contatos.addEventListener('click', (event) => { 
+    if($mobileMenu.children[0].classList.contains('fa-xmark')) toggleMenu(event)
+})
+
 $linkSobreMim.addEventListener('click', sobreMim)
 $botaoFechar.addEventListener('click', botaoFechar)
+
+window.addEventListener("resize", debounce(() => {
+    if($mobileMenu.children[0].classList.contains('fa-xmark')) toggleMenu()
+}))
+
+atualizarAnoRodape()
+
