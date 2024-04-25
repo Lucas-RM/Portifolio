8a7c23285e81a0b71526d86e8e1fb9094eaad4f3
Lucas Marcondes <lucas.marcondes36@gmail.com>
2024-04-25 14:35:17 -0300

    Adicionando o arquivo de dados da seção Experiência da página Currículo

---
+++ script/dadosExperiencia.js
@@ -0,0 +1,20 @@
+[
+    {
+        "img": "https://media.licdn.com/dms/image/C4D0BAQFto4Qo1rCVhg/company-logo_100_100/0/1656687748687/sinqia_logo?e=1721865600&v=beta&t=TOkYQUR6tHt9iAmwV9CkKvAaxqOL4NNwEioUo2Sthrc",
+        "titulo": "sinqia",
+        "paragrafo": [
+            "Tempo integral • 1 ano e 6 meses",
+            "São Paulo, Brasil • Híbrida"
+        ],
+        "cargos": [
+            "Auxiliar Dev I",
+            "Auxiliar Dev II"
+        ],
+        "descricao": [
+            "Atuação nas principais soluções voltadas a Previdência Privada Complementar tanto em manutenções quanto em evoluções de software.<br>",
+            "- Desenvolvimento de projetos utilizando: C#; .NET Core; ASP.NET MVC; HTML/CSS; Angular; JavaScript; Entity Framework Core; e SQL Server.",
+            "- Desenvolvimento de projetos;",
+            "- Metodologias ágeis como SCRUM e Kanban."
+        ]
+    }
+]
