62a7518d480f9231a829d4821d60c223a98419e6
Lucas-RM <lucas.marcondes36@gmail.com>
2024-10-06 01:54:02 -0300

    Modificações feitas na seção Meus Projetos

--- script/dadosDosProjetos.js
+++ script/dadosDosProjetos.js
@@ -1,55 +1,91 @@
 const projetos = [
     {
+        link: 'https://lucas-rm.github.io/gitfind-react-cli/',
+        img: '../img/projetos/git-find.jpg',
+        titulo: 'git find',
+        descricao: 'Este projeto, desenvolvido em <b>React</b>, utiliza a <b>API do GitHub</b> para permitir a <u>busca de perfis de usuários e seus respectivos repositórios</u>. A aplicação permite que os usuários insiram o nome de um perfil do GitHub e, ao realizar a busca, exibe na tela informações básicas do perfil, além de uma lista de repositórios públicos associados a esse usuário.'
+    },
+    {
+        link: 'https://lucas-rm.github.io/minimal-api/',
+        img: '../img/projetos/padrao-gitHub-logo.jpg',
+        titulo: 'Minimal API',
+        descricao: 'Neste desafio criei uma <b>API</b> utilizando a técnica de <b>Minimals APIs</b> para o <u>registro de veículos</u>, ampliando suas funcionalidades ao incorporar <u>administradores</u> com regras <b>JWT</b>. Ao explorar o funcionamento da API, familiarizarei com o uso do <b>Swagger</b>, além de trabalhar com testes, garantindo a robustez e confiabilidade do sistema que desenvolvi.'
+    },
+    {
+        link: 'https://github.com/Lucas-RM/trilha-net-api-desafio/',
+        img: '../img/projetos/padrao-gitHub-logo.jpg',
+        titulo: 'gerenciador de tarefas',
+        descricao: 'Repositório criado para o desenvolvimento do desafio em que foi solicitado para construir um <b>Sistema Gerenciador de Tarefas</b>, onde eu poderia cadastrar uma lista de tarefas que permitiria organizar melhor a minha rotina. Essa lista de tarefas precisa ter um <b>CRUD</b> e a aplicação deverá ser do tipo Web API ou MVC. No caso, construí uma <b>Web API</b>.'
+    },
+    {
+        link: 'https://lucas-rm.github.io/agenda/',
+        img: '../img/projetos/projeto-agenda.jpg',
+        titulo: 'projeto agenda',
+        descricao: 'O projeto <b>Agenda</b> é um site que as pessoas podem definir o ano, o mês, o dia e o horário de suas tarefas. Além disso, terá as funções de deletar e de marcar como concluído as tarefas finalizadas.'
+    },
+    {
+        link: 'https://github.com/Lucas-RM/calculadora-react-desafio-dio/',
+        img: '../img/projetos/padrao-gitHub-logo.jpg',
+        titulo: 'calculadora (react)',
+        descricao: 'Projeto criado para solucionar o desafio de criar uma <b>Calculadora</b> utilizando a biblioteca <b>React</b> no curso feito na plataforma DIO.'
+    },
+    {
+        link: 'https://github.com/Lucas-RM/calculadora-testes-unitarios/',
+        img: '../img/projetos/padrao-gitHub-logo.jpg',
+        titulo: 'calculadora (testes unitários)',
+        descricao: 'Projeto feito com intuito de aprender sobre <b>testes unitários</b>. Criação de uma calculadora simples e montando testes para os métodos da classe Calculadora.'
+    },
+    {
+        link: 'https://lucas-rm.github.io/pesquisa_inclusao_digital/',
+        img: '../img/projetos/Projeto-InclusaoDigital.jpg',
+        titulo: 'projeto inclusão digital',
+        descricao: 'Projeto acadêmico realizado em grupo na faculdade com a finalidade de compreender o processo de <b>Inclusão Digital</b> na sociedade.'
+    },
+    {
+        link: 'https://github.com/Lucas-RM/Editor-de-Texto/',
+        img: '../img/projetos/editor-de-texto.jpg',
+        titulo: 'editor de texto',
+        descricao: 'Projeto acadêmico feito em curso com intuito de assemelhar-se com um editor de texto básico.'
+    },
+    {
+        link: 'https://lucas-rm.github.io/relogio-online/',
+        img: '../img/projetos/Projeto-Relogio.jpg',
+        titulo: 'projeto relógio',
+        descricao: 'Projeto com intuito de representar um relógio em um site.'
+    },
+    {
         link: 'https://lucas-rm.github.io/alura-midi/',
-        img: './img/alura-midi.jpg',
-        titulo: 'Alura MIDI',
+        img: '../img/projetos/alura-midi.jpg',
+        titulo: 'alura MIDI',
         descricao: 'Site feito em curso da plataforma Alura com o objetivo de tocar sons de instrumentos quando clicar em um botão ou quando pressionar a tecla <b>SPACE</b>.'
     },
     {
         link: 'https://lucas-rm.github.io/projeto-apeperia/',
-        img: './img/projeto-apeperia.jpg',
+        img: '../img/projetos/projeto-apeperia.jpg',
         titulo: 'projeto apeperia',
         descricao: 'Site feito em curso da plataforma Alura com intuito de aprender sobre <b>Layouts Responsivos (mobile)</b>.'
     },
     {
         link: 'https://lucas-rm.github.io/alura-flexbox/',
-        img: './img/alura-flexbox.jpg',
+        img: '../img/projetos/alura-flexbox.jpg',
         titulo: 'alura flexbox',
         descricao: 'Site feito em curso da plataforma Alura com intuito de aprender sobre <b>Flexbox</b>.'
     },
     {
         link: 'https://lucas-rm.github.io/Projeto-Huddle/',
-        img: './img/Projeto-Huddle.jpg',
+        img: '../img/projetos/Projeto-Huddle.jpg',
         titulo: 'projeto huddle',
-        descricao: 'Projeto acadêmico feito em curso com objetivo de aplicar <b>posicionamento do conteúdo</b> do site e deixar o site <b>responsivo</b>, sendo compatível para celular, computador, ipads, entre outros dispositivos.'
-    },
-    {
-        link: 'https://lucas-rm.github.io/relogio-online/',
-        img: './img/Projeto-Relogio.jpg',
-        titulo: 'projeto relógio',
-        descricao: 'Projeto com intuito de representar um relógio em um site.'
-    },
-    {
-        link: 'https://lucas-rm.github.io/pesquisa_inclusao_digital/',
-        img: './img/Projeto-InclusaoDigital.jpg',
-        titulo: 'projeto inclusão digital',
-        descricao: 'Projeto acadêmico realizado em grupo na faculdade com a finalidade de compreender o processo de <b>Inclusão Digital</b> na sociedade.'
+        descricao: 'Projeto acadêmico feito em curso com objetivo de aplicar <b>posicionamento do conteúdo</b> do site e deixar o site <b>responsivo</b>, sendo compatível para celular, computador, iPads, entre outros dispositivos.'
     },
     {
         link: 'https://lucas-rm.github.io/alura-store/',
-        img: './img/alura-store.jpg',
+        img: '../img/projetos/alura-store.jpg',
         titulo: 'alura store',
-        descricao: 'Site feito em curso da plataforma Alura com o objetivo de aprender sobre <b>CSS Grid</b> e a <b>simplicação de Layouts</b>.'
-    },
-    {
-        link: 'https://lucas-rm.github.io/agenda/',
-        img: './img/projeto-agenda.jpg',
-        titulo: 'projeto agenda',
-        descricao: 'O projeto agenda é um site que as pessoas podem definir o ano, o mês, o dia e o horário de suas tarefas. Além disso, terá as funções de deletar e de marcar como concluído as tarefas finalizadas.'
+        descricao: 'Site feito em curso da plataforma Alura com o objetivo de aprender sobre <b>CSS Grid</b> e a <b>simplificação de Layouts</b>.'
     },
     {
         link: 'https://lucas-rm.github.io/teclado/',
-        img: './img/teclado-teclas.jpg',
+        img: '../img/projetos/teclado-teclas.jpg',
         titulo: 'teclado (código da tecla)',
         descricao: 'Se você não sabe qual a numeração (ou keyCode) das teclas do seu teclado e quer saber o mais rápido possível, acesse esse site e descubra.'
     }
