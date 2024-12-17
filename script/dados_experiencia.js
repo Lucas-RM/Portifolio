7b13e849519a7a87d9ea49ad2a5c75cfd382c2cf
Lucas-RM <lucas.marcondes36@gmail.com>
2024-12-16 21:36:25 -0300

    Alteração na descrição da seção Experiência da empresa 'Sinqia' na página Currículo.

--- script/dados_experiencia.js
+++ script/dados_experiencia.js
@@ -14,6 +14,7 @@
             "Atuação nas principais soluções voltadas a <b>Previdência Privada Complementar</b> tanto em manutenções quanto em evoluções de software.<br><br>",
             "- Colaborou com equipes multifuncionais para desenvolver e implantar soluções financeiras de alta qualidade, acomodando legislação em rápida mudança para mais de 200.000 usuários divididos entre mais de 100 empresas clientes.",
             "- Atuação no time de Engenharia N2, efetuando melhorias e correções/manutenções os projetos de mais de 27 clientes.",
+            "- Implementei a funcionalidade 'Instituto de Cancelamento' contribuindo para a melhoria dos processos de gestão e integração de sistemas, além de atuar com outros institutos como BPD e Autopatrocínio.",
             "- Desenvolvimento de integrações com plataformas como TFS/Azure DevOps e Jira.",
             "- Metodologias ágeis como Scrum e Kanban.<br><br>",
             "<b>Tecnologias:</b> C# | .NET Core | ASP.NET MVC | HTML/CSS | Angular | JavaScript | Entity Framework Core | GIT | SQL | SQL Server."
